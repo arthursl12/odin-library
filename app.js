57bac079c6f83ce74e3d38bd4d55a0e6793bf6ca
arthursl12 <arthursoutolima@gmail.com>
2023-02-26 08:20:10 -0300

    Create display books and add to library

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -16,5 +18,43 @@ function Book(title, author, pages, read) {
   };
 }
 
-let theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
-console.log(theHobbit.info());
+function addBookToLibrary(b) {
+  return myLibrary.push(b);
+}
+
+function displayBooks() {
+  // Clear current display
+  let booklist = document.querySelector("#booklist");
+  if (booklist.hasChildNodes()) {
+    booklist.childNodes.forEach((child) => booklist.removeChild(child));
+  }
+
+  // Add an entry for each book
+  myLibrary.forEach((book) => {
+    let bookcard = document.createElement("div");
+
+    // Loop through all properties of Book
+    Object.keys(book).forEach((key) => {
+      if (!(typeof book[key] === "function")) {
+        // Only add non-function properties
+        let newp = document.createElement("p");
+        newp.textContent = `${book[key]}`;
+        newp.classList.add(key);
+        bookcard.appendChild(newp);
+      }
+    });
+    booklist.appendChild(bookcard);
+  });
+}
+
+addBookToLibrary(new Book("The Hobbit", "J.R.R Tolkien", 295, false));
+addBookToLibrary(
+  new Book("Memórias Póstumas de Brás Cubas", "Machado de Assis", 320, false)
+);
+addBookToLibrary(
+  new Book("20,000 Leagues Under the Sea", "Jules Verne", 316, true)
+);
+
+displayBooks();
+
+console.log(myLibrary);
